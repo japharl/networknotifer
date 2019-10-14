651a7d6b75f3ca70c84e7b6b08d349fdecdf5c01
Zak Zebrowski <zak.zebrowski@gmail.com>
2019-10-14 17:30:00 -0400

    Create down.py

---
+++ down.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from PIL import ImageFont
+import sys
+from datetime import datetime
+
+import inkyphat
+
+colour = "red"
+
+try:
+    inkyphat.set_colour(colour)
+except ValueError:
+    print('Invalid colour "{}" for V{}\n'.format(colour, inkyphat.get_version()))
+    if inkyphat.get_version() == 2:
+        sys.exit(1)
+    print('Defaulting to "red"')
+
+font_file = inkyphat.fonts.FredokaOne
+inkyphat.arc((0, 0, 212, 104), 0, 180, 2)
+
+top = 0
+left = 0
+offset_left = 0
+now = datetime.now()
+dt = now.strftime("%m/%d/%Y %H:%M")
+text = "As of :\n"
+text += dt
+text += "\nthe network is down."
+
+font_size=20
+font = inkyphat.ImageFont.truetype(font_file, font_size)
+width, height = font.getsize(text)
+inkyphat.text((0, top), text, inkyphat.RED, font=font)
+
+inkyphat.show()
